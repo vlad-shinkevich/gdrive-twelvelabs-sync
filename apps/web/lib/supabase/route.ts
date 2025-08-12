a8efb5040a3cfb7cbc00478b05b387720e06aa78
Vlad Shinkevich <vladislav.shinkevich@bpmobile.com>
2025-08-12 17:06:01 +0300

    fix: correct arguments for cookieStore.delete

--- apps/web/lib/supabase/route.ts
+++ apps/web/lib/supabase/route.ts
@@ -17,7 +17,7 @@ export async function createRouteSupabase() {
         cookieStore.set(name, value, options)
       },
       remove(name: string, options: CookieOptions) {
-        cookieStore.delete(name, options)
+        cookieStore.delete({ name, ...options })
       },
     },
   })
